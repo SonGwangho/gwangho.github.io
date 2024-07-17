73990e8d9593bda1714a6ae15c07b94c713ac4ef
ghson <ghson@ocube.co.kr>
2024-07-17 10:07:16 +0900

    ok일때까지 받아오게 수정..

--- js/pokemon.js
+++ js/pokemon.js
@@ -3,8 +3,8 @@ class Pokemon {
     if (!url) {
       url = "https://pokeapi.co/api/v2/pokemon";
     }
-    const response = await fetch(url);
-    if (url.indexOf("218")) debugger;
+    let response = await fetch(url);
+    while (!response.ok) response = await fetch(url);
     const data = await response.json();
     return data;
   }

d3e5c39cf0bf3cc287c4a772117e9266bd508189
ghson <ghson@ocube.co.kr>
2024-07-19 12:51:54 +0900

    테스트를 위해 커밋

--- js/pokemon.js
+++ js/pokemon.js
@@ -86,7 +86,7 @@ async function loadPokemons() {
   };
 
   const div = document.getElementById("pokemon");
-  const ol = document.getElementById("pokeList");
+  const ol = document.getElementById("poke_list");
   div.appendChild(ol);
   let cnt = ol.childElementCount;
   let url = `https://pokeapi.co/api/v2/pokemon?offset=${cnt}&limit=40`;
@@ -109,82 +109,7 @@ async function loadPokemons() {
       continue;
     }
     const li = document.createElement("div");
-    const pokemonDiv = document.createElement("div");
-
-    const img = document.createElement("img");
-    const infoDiv = document.createElement("div");
-    const name = document.createElement("label");
-    const types = document.createElement("div");
-    const abilities = document.createElement("div");
-    const stats = document.createElement("div");
-
-    li.classList.add("pokemon_outer_div");
-
-    pokemonDiv.classList.add("pokemon_inner_div");
-
-    img.src = parsed.sprites.front_default;
-    img.classList.add("pokemon_img");
-
-    infoDiv.classList.add("pokemon_info");
-
-    name.innerText = `${parsed.name} No.${number.toString()}`;
-    name.style.fontSize = "40px";
-    name.style.fontWeight = "700";
-    name.style.marginBottom = "15px";
-
-    types.classList.add("pokemon_area");
-    parsed.types.forEach((t) => {
-      const type = document.createElement("label");
-      type.innerText = t.name;
-      type.style.color = typeColor[t.name];
-      type.classList.add("pokemon_item");
-      types.appendChild(type);
-    });
-
-    abilities.classList.add("pokemon_area");
-    parsed.abilities.forEach((ab) => {
-      const ability = document.createElement("label");
-      ability.innerText = ab.name;
-      ability.classList.add("pokemon_item");
-      if (ab.is_hidden) {
-        ability.classList.add("pokemon_hidden_ability");
-      }
-      abilities.appendChild(ability);
-    });
-
-    const statrow = [
-      document.createElement("div"),
-      document.createElement("div"),
-    ];
-    parsed.stats.forEach((s) => {
-      const stat = document.createElement("div");
-      const n = document.createElement("label");
-      n.innerText = s.name + "  ";
-      const bs = document.createElement("label");
-      bs.innerText = s.base_stat;
-      if (parseInt(s.base_stat) >= 130) {
-        bs.style.color = "red";
-      }
-      stat.appendChild(n);
-      stat.appendChild(bs);
-      stat.classList.add("pokemon_item");
-      if (statrow[0].childElementCount < 3) {
-        statrow[0].appendChild(stat);
-      } else {
-        statrow[1].appendChild(stat);
-      }
-    });
-    stats.appendChild(statrow[0]);
-    statrow[0].classList.add("pokemon_area");
-    stats.appendChild(statrow[1]);
-    statrow[1].classList.add("pokemon_area");
-
-    pokemonDiv.appendChild(img);
-    infoDiv.appendChild(name);
-    infoDiv.appendChild(types);
-    infoDiv.appendChild(abilities);
-    infoDiv.appendChild(stats);
-    pokemonDiv.appendChild(infoDiv);
+    pokemonDiv = getPokemonDiv(parsed);
     li.appendChild(pokemonDiv);
     fragment.appendChild(li);
   }
@@ -204,5 +129,137 @@ function handleScroll(entries, observer) {
     if (!loading) {
       loadPokemons();
     }
+  }
+}
+
+function getPokemonDiv(parsed) {
+  const pokemonDiv = document.createElement("div");
+
+  const img = document.createElement("img");
+  const infoDiv = document.createElement("div");
+  const name = document.createElement("label");
+  const types = document.createElement("div");
+  const abilities = document.createElement("div");
+  const stats = document.createElement("div");
+
+  li.classList.add("pokemon_outer_div");
+
+  pokemonDiv.classList.add("pokemon_inner_div");
+
+  img.src = parsed.sprites.front_default;
+  img.classList.add("pokemon_img");
+
+  infoDiv.classList.add("pokemon_info");
+
+  name.innerText = `${parsed.name} No.${number.toString()}`;
+  name.style.fontSize = "40px";
+  name.style.fontWeight = "700";
+  name.style.marginBottom = "15px";
+
+  types.classList.add("pokemon_area");
+  parsed.types.forEach((t) => {
+    const type = document.createElement("label");
+    type.innerText = t.name;
+    type.style.color = typeColor[t.name];
+    type.classList.add("pokemon_item");
+    types.appendChild(type);
+  });
+
+  abilities.classList.add("pokemon_area");
+  parsed.abilities.forEach((ab) => {
+    const ability = document.createElement("label");
+    ability.innerText = ab.name;
+    ability.classList.add("pokemon_item");
+    if (ab.is_hidden) {
+      ability.classList.add("pokemon_hidden_ability");
+    }
+    abilities.appendChild(ability);
+  });
+
+  const statrow = [
+    document.createElement("div"),
+    document.createElement("div"),
+  ];
+  parsed.stats.forEach((s) => {
+    const stat = document.createElement("div");
+    const n = document.createElement("label");
+    n.innerText = s.name + "  ";
+    const bs = document.createElement("label");
+    bs.innerText = s.base_stat;
+    if (parseInt(s.base_stat) >= 130) {
+      bs.style.color = "red";
+    }
+    stat.appendChild(n);
+    stat.appendChild(bs);
+    stat.classList.add("pokemon_item");
+    if (statrow[0].childElementCount < 3) {
+      statrow[0].appendChild(stat);
+    } else {
+      statrow[1].appendChild(stat);
+    }
+  });
+  stats.appendChild(statrow[0]);
+  statrow[0].classList.add("pokemon_area");
+  stats.appendChild(statrow[1]);
+  statrow[1].classList.add("pokemon_area");
+
+  pokemonDiv.appendChild(img);
+  infoDiv.appendChild(name);
+  infoDiv.appendChild(types);
+  infoDiv.appendChild(abilities);
+  infoDiv.appendChild(stats);
+  pokemonDiv.appendChild(infoDiv);
+
+  return pokemonDiv;
+}
+
+async function getDatas() {
+  if (MyStorage.getSessionData("pokemon_ko")) {
+    let response_ko = await fetch("./assets/pokemon_ko.json");
+    let kson = await response_ko.json();
+    MyStorage.saveSession("pokemon_ko", kson);
+  }
+
+  if (MyStorage.getSessionData("pokemon_convert")) {
+    let response_convert = await fetch("./assets/pokemon_convert.json");
+    let cson = await response_convert.json();
+    MyStorage.saveSession("pokemon_convert", cson);
+  }
+}
+
+function onInput(e) {
+  try {
+    const data = MyStorage.getSessionData("pokemon_ko");
+    let value = e.tartget.innerText;
+    if (data[value].length < 1) return;
+    let pokemons = data[value].slice(0, 5);
+    const resultDiv = document.getElementById("search_result_div");
+    while (resultDiv.childElementCount > 0) {
+      resultDiv.removeChild(resultDiv.children[0]);
+    }
+    for (let pokemon of pokemons) {
+      const div = document.createElement("div");
+      div.classList.add("search_item");
+      const span = document.createElement("span");
+      span.innerText = pokemon;
+
+      div.appendChild(span);
+      resultDiv.appendChild(div);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function search() {
+  const text = document.getElementsByClassName("search_input")[0].innerText;
+  const converter = MyStorage.getSessionData("pokemon_convert");
+  if (!converter[text]) {
+    const url = "https://pokeapi.co/api/v2/pokemon/" + converter[text];
+    const pokemon = await Pokemon.getPokemons(url);
+    const parsed = await Pokemon.parsing(pokemon);
+    const div = getPokemonDiv(parsed);
+
+    Modal.showModal(div.innerHTML);
   }
 }

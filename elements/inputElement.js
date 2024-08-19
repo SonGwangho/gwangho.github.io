e8ace8db6cdf96c14d2266e020cca1e5b34dbd4b
ghson <ghson@ocube.co.kr>
2024-08-19 11:21:03 +0900

    수정수정

--- elements/inputElement.js
+++ elements/inputElement.js
@@ -4,36 +4,41 @@ class InputFile extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const style = document.createElement("style");
     style.textContent = `
-    {
       button:hover {
         font-weight: 700;
-      }
-    }`;
+      }`;
     shadow.appendChild(style);
 
     const input = document.createElement("input");
     input.type = "file";
-    input.style.display = "none"; // 기본 파일 입력 숨기기
+    input.style.display = "none";
 
     const button = document.createElement("button");
-    button.textContent = "백업했던 json 파일을 업로드 해주세요.";
+    button.textContent = "파일 선택";
     button.style.padding = "3px 5px";
     button.style.backgroundColor = "transparent";
     button.style.border = "0";
     button.style.cursor = "pointer";
     button.style.margin = "0 10px";
     button.style.fontFamily = "'Ownglyph_noocar-Rg', sans-serif";
+    button.style.fontSize = "18px";
 
     button.addEventListener("click", () => {
       input.click();
     });
 
-    input.addEventListener("change", () => {
+    input.addEventListener("change", (event) => {
       const fileName =
         input.files.length > 0
           ? input.files[0].name
           : "파일이 선택되지 않았습니다.";
       button.textContent = fileName;
+
+      this.dispatchEvent(new Event("change"));
+      if (this.getAttribute("onchange")) {
+        const onchangeHandler = new Function(this.getAttribute("onchange"));
+        onchangeHandler.call(this, event);
+      }
     });
 
     shadow.appendChild(button);

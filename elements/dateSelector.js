03521aeca850d6de53a62bcf975c124aff7ea68e
ghson <ghson@ocube.co.kr>
2024-09-02 15:23:32 +0900

    날짜 선택하는거 만들었음

--- elements/dateSelector.js
+++ elements/dateSelector.js
@@ -1,6 +1,9 @@
 class dateCalendar extends HTMLElement {
   constructor() {
     super();
+    this.today = null;
+    this.todayString = null;
+    this.caption = document.createElement("caption");
     this.setToday(new Date());
   }
 
@@ -17,8 +20,46 @@ class dateCalendar extends HTMLElement {
   }
 
   renderCalendar() {
-    this.shadow = null;
-    this.shadow = this.attachShadow({ mode: "closed" });
+    if (!this.shadow) {
+      this.shadow = this.attachShadow({ mode: "open" });
+    }
+    while (this.shadow?.firstChild) {
+      this.shadow.removeChild(this.shadow.firstChild);
+    }
+    const root = document.createElement("div");
+
+    this.shadow.appendChild(root);
+
+    const style = document.createElement("style");
+    style.textContent = `
+      #my_calendar {
+        background: lightblue;
+        color: black;
+        width: 100%;
+        text-align: center;
+        max-width: 800px;
+        margin: auto;
+        min-height: 400px;
+      }
+      th, td {
+        font-weight: bold;
+      }
+      td {
+        cursor: pointer;
+      }
+      .thead_row {
+        font-size: 1.3em;
+        margin-bottom: 30px;
+        height: auto;
+        border-bottom: 1.8px solid black;
+      }
+      .clicked_calendar {
+        color: red !important;
+      }
+      .calendar_month_arrow{
+        font-size: 20px;
+      }
+    `;
     const now = this.today;
 
     const monthNames = [
@@ -37,34 +78,60 @@ class dateCalendar extends HTMLElement {
     ];
 
     const monthArea = document.createElement("div");
+    monthArea.style.display = "flex";
 
     const month = document.createElement("h1");
     month.style.textAlign = "center";
     month.style.color = "orange";
     month.style.display = "inline-block";
     month.innerText = monthNames[now.getMonth()];
+    month.style.margin = "0 auto";
+    month.style.flexGrow = "1";
 
     const beforeMonth = document.createElement("button");
+    beforeMonth.classList.add("calendar_month_arrow");
     beforeMonth.style.background = "transparent";
     beforeMonth.style.border = "0";
     beforeMonth.style.display = "inline-block";
-    beforeMonth.innerText = "<";
+    beforeMonth.innerText = "←";
+
+    beforeMonth.addEventListener("click", () => {
+      const newMonth = new Date(now);
+      newMonth.setMonth(newMonth.getMonth() - 1);
+      if (newMonth.getDate() !== now.getDate()) {
+        newMonth.setDate(0);
+      }
+      this.setToday(newMonth);
+      this.renderCalendar();
+    });
 
     const afterMonth = document.createElement("button");
+    afterMonth.classList.add("calendar_month_arrow");
     afterMonth.style.background = "transparent";
     afterMonth.style.border = "0";
     afterMonth.style.display = "inline-block";
-    afterMonth.innerText = ">";
+    afterMonth.innerText = "→";
+
+    afterMonth.addEventListener("click", () => {
+      const newMonth = new Date(now);
+      newMonth.setMonth(newMonth.getMonth() + 1);
+      if (newMonth.getDate() !== now.getDate()) {
+        newMonth.setDate(0);
+      }
+      this.setToday(newMonth);
+      this.renderCalendar();
+    });
 
     monthArea.appendChild(beforeMonth);
     monthArea.appendChild(month);
     monthArea.appendChild(afterMonth);
 
     const table = document.createElement("table");
-    this.caption = document.createElement("caption");
-    caption.style.captionSide = "bottom";
-    caption.style.fontSize = "30px";
-    caption.style.paddingLeft = "20px";
+    table.style.margin = "auto";
+    this.caption.style.captionSide = "bottom";
+    this.caption.style.fontSize = "30px";
+    this.caption.style.paddingLeft = "20px";
+    this.caption.style.display = "none";
 
     const tableHead = document.createElement("thead");
     let headRow = document.createElement("tr");
@@ -87,7 +154,7 @@ class dateCalendar extends HTMLElement {
     const tableBody = document.createElement("tbody");
     let tr = document.createElement("tr");
     while (weekdays.length > 0) {
-      weekday = weekdays.shift();
+      const weekday = weekdays.shift();
       tr.classList.add("tbody_row");
       if (tableBody.children.length < 1 && tr.children.length < 1) {
         for (let i = 0; i < weekday.weekdayNumber; i++) {
@@ -126,18 +193,28 @@ class dateCalendar extends HTMLElement {
         td.style.color = "blue";
       }
 
+      td.addEventListener("click", (event) => {
+        const clickedDay = event.target.innerText;
+        this.setToday(new Date(now).setDate(clickedDay));
+        this.dispatchEvent(
+          new Event("click", { bubbles: true, composed: true })
+        );
+        this.renderCalendar();
+      });
+
       tr.appendChild(td);
     }
     tableBody.appendChild(tr);
 
     tableHead.appendChild(headRow);
-    table.appendChild(caption);
+    table.appendChild(this.caption);
     table.appendChild(tableHead);
     table.appendChild(tableBody);
 
-    this.shadow.appendChild(monthArea);
-    this.shadow.appendChild(table);
-    this.shadow.appendChild(document.createElement("br"));
+    root.appendChild(style);
+    root.appendChild(monthArea);
+    root.appendChild(table);
+    root.appendChild(document.createElement("br"));
   }
 
   getDayWithweekdays(date) {
@@ -151,7 +228,7 @@ class dateCalendar extends HTMLElement {
     const lastDayOfMonth = new Date(firstDayOfNextMonth - 1);
     const daysOfWeek = "일월화수목금토".split("");
 
-    lst = [];
+    const lst = [];
     for (
       let day = firstDayOfMonth;
       day <= lastDayOfMonth;
@@ -165,9 +242,9 @@ class dateCalendar extends HTMLElement {
     return lst;
   }
 
-  set setToday(datetime) {
+  setToday(datetime) {
     try {
-      const date = new Date(Date.parse(datetime));
+      const date = new Date(datetime);
       this.today = date;
 
       const year = date.getFullYear();
@@ -197,8 +274,12 @@ class dateCalendar extends HTMLElement {
     }
   }
 
-  get today() {
+  get getToday() {
     return this.todayString;
+  }
+
+  get tds() {
+    return this.shadow.querySelectorAll("td");
   }
 }
 
--- elements/inputElement.js
+++ elements/inputElement.js
@@ -59,8 +59,144 @@ class InputFile extends HTMLElement {
 }
 customElements.define("input-file", InputFile);
 
+/**
+ * 아래처럼 사용
+ * element.changeEventCallback = (date) => {}
+ *
+ * element.selectedDate 여기에 날짜 들어있음
+ */
 class InputDate extends HTMLElement {
   constructor() {
     super();
+    this._selectedDate = null;
+    this._width = 200;
+    this._height = 200;
+    this.dateView = document.createElement("div");
+    const shadow = this.attachShadow({ mode: "closed" });
+
+    const style = document.createElement("style");
+    style.textContent = `
+      .inputDate {
+        font-size: ${this._width * 0.17}px;
+        cursor: pointer;
+      }
+      span {
+        margin: 2px 2px;
+        font-size: ${this._width * 0.12}px;
+      }`;
+    shadow.appendChild(style);
+
+    const year = document.createElement("span");
+    year.classList.add("inputDate", "year");
+    const month = document.createElement("span");
+    month.classList.add("inputDate", "month");
+    const day = document.createElement("span");
+    day.classList.add("inputDate", "day");
+    const dash1 = document.createElement("span");
+    dash1.textContent = "-";
+    const dash2 = document.createElement("span");
+    dash2.textContent = "-";
+
+    this.dateView.appendChild(year);
+    this.dateView.appendChild(dash1);
+    this.dateView.appendChild(month);
+    this.dateView.appendChild(dash2);
+    this.dateView.appendChild(day);
+
+    year.addEventListener("click", () => {});
+
+    const calendarDiv = document.createElement("div");
+    calendarDiv.classList.add("inputDate_calendar_div");
+    const calendar = document.createElement("date-calendar");
+    calendar.classList.add("inputDate_calendar");
+
+    this.dateView.style.width = `${this._width}px`;
+    this.dateView.style.height = `fit-content`;
+    calendarDiv.style.width = `${this._width}px`;
+    calendarDiv.style.height = `fit-content`;
+
+    calendar.style.width = `100%`;
+    calendar.style.height = `${this._height}px`;
+
+    calendar.addEventListener("click", (event) => {
+      this.selectedDate = calendar.getToday;
+    });
+
+    calendarDiv.appendChild(calendar);
+    calendarDiv.style.display = "none";
+    calendarDiv.style.position = "absolute";
+
+    this.dateView.addEventListener("click", () => {
+      calendarDiv.style.display === "block"
+        ? (calendarDiv.style.display = "none")
+        : (calendarDiv.style.display = "block");
+    });
+
+    shadow.appendChild(this.dateView);
+    shadow.appendChild(calendarDiv);
+
+    this.init();
+  }
+
+  init() {
+    this.selectedDate = new Date();
+  }
+
+  setYear() {
+    const year = this.dateView.querySelector(".year");
+    year.textContent = this.getYear();
+  }
+
+  getYear() {
+    return new Date(this._selectedDate).getFullYear();
+  }
+
+  setMonth() {
+    const month = this.dateView.querySelector(".month");
+    month.textContent = this.getMonth();
+  }
+
+  getMonth() {
+    const dateStr = "00" + (new Date(this._selectedDate).getMonth() + 1);
+    return dateStr.substring(dateStr.length - 2, dateStr.length);
+  }
+
+  setDay() {
+    const day = this.dateView.querySelector(".day");
+    day.textContent = this.getDay();
+  }
+
+  getDay() {
+    const dateStr = "00" + new Date(this._selectedDate).getDate();
+    return dateStr.substring(dateStr.length - 2, dateStr.length);
+  }
+
+  set selectedDate(date) {
+    const newDate = new Date(date);
+    this._selectedDate = newDate;
+    this.setYear();
+    this.setMonth();
+    this.setDay();
+  }
+
+  get selectedDate() {
+    return this._selectedDate;
+  }
+
+  set changeEventCallback(callback) {
+    callback(this._selectedDate);
+  }
+
+  static get observedAttributes() {
+    return ["width", "height"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "width") {
+      this._width = newValue;
+    } else if (name === "height") {
+      this._height = newValue;
+    }
   }
 }
+customElements.define("input-date", InputDate);
